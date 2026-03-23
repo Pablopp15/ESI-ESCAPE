dd4389bf9e27e3e10eeb3af949f1eb712988b787
NoobTrajeado23 <angel.bermudezortega@alum.uca.es>
2026-03-23 17:34:07 +0100

    Update Partida.h

--- Partida.h
+++ Partida.h
@@ -2,6 +2,8 @@
 #define PARTIDA_H
 
 void menu_principal(Jugador lista_jugadores[], int *num_jugadores);
-void menu(char id_jugador[], int id_sala_actual);
+void nueva_partida(char id_jugador[]);
+void partida(char id_jugador[], int id_sala_actual);
+
 
 #endif
