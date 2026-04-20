771739a3c7e9ffd8ddaef96fcd67ac33e8abc269
Sr-Name <gonzalogilduque2005@gmail.com>
2026-04-20 13:16:14 +0200

    Add files via upload

--- ficheros.c
+++ ficheros.c
@@ -1,107 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define MAX_PUZLES 100
-#define MAX_SALAS 100
-#define MAX_CONEXIONES 100
-#define MAX_OBJETOS 100
-#define MAX_JUGADORES 100
-
-#define CARGAR 0
-#define GUARDAR 1
-
-/* ==================== ESTRUCTURAS ==================== */
-
-typedef struct {
-    char id[4];
-    char nombre[16];
-    int id_sala;
-    char tipo[10];
-    char descripcion[151];
-    char solucion[51];
-} Puzle;
-
-typedef struct {
-    int id;
-    char nombre[31];
-    char tipo[10];
-    char descripcion[151];
-} Sala;
-
-typedef struct {
-    char id[4];
-    int origen;
-    int destino;
-    char estado[15];
-    char condicion[10];
-} Conexion;
-
-typedef struct {
-    char id[5];
-    char nombre[16];
-    char descripcion[51];
-    char localizacion[15];
-} Objeto;
-
-typedef struct {
-    int id;
-    char nombre[21];
-    char usuario[11];
-    char password[9];
-} Jugador;
-
+#include "Ficheros.h"
 
 /* ========================================================= */
-/* FUNCION PARA LEER UN FICHERO DE TEXTO LINEA A LINEA */
+/* FUNCION PARA LEER UN FICHERO DE TEXTO LINEA A LINEA       */
 /* ========================================================= */
+int leer_fichero(char *nombreFichero, char texto[][300]) {
+    FILE *f;      
+    int i = 0;    
 
-int leer_fichero(char *nombreFichero, char texto[][100]) {
-
-    FILE *f;      // Puntero al fichero
-    int i = 0;    // Contador de líneas
-
-    // Abrimos el fichero en modo lectura
     f = fopen(nombreFichero, "r");
 
-    // Si hay error
     if (f == NULL) {
-        printf("Error al abrir el fichero.\n");
+        printf("Error al abrir el fichero %s.\n", nombreFichero);
         return 0;
     }
 
-    // Leemos línea a línea
-    while (fgets(texto[i], 100, f) != NULL) {
-
-        // Quitamos el salto de línea '\n'
+    while (fgets(texto[i], 300, f) != NULL) {
         texto[i][strcspn(texto[i], "\n")] = '\0';
-
         i++;
     }
 
-    fclose(f);  // Cerramos fichero
-
-    return i;   // Devolvemos número de líneas leídas
+    fclose(f);  
+    return i;   
 }
 
-
 /* ========================================================= */
-/* FUNCION PARA ESCRIBIR AL FINAL DE UN FICHERO */
+/* FUNCION PARA ESCRIBIR AL FINAL DE UN FICHERO              */
 /* ========================================================= */
-
-void escribir_fichero(char *nombreFichero, char texto[][100], int numLineas) {
-
+void escribir_fichero(char *nombreFichero, char texto[][300], int numLineas) {
     FILE *f;
 
-    // Abrimos en modo append (añadir al final)
     f = fopen(nombreFichero, "a");
 
     if (f == NULL) {
-        printf("Error al abrir el fichero.\n");
+        printf("Error al abrir el fichero %s para escritura.\n", nombreFichero);
         return;
     }
 
-    // Escribimos cada línea
     for (int i = 0; i < numLineas; i++) {
         fprintf(f, "%s\n", texto[i]);
     }
@@ -109,17 +46,11 @@ void escribir_fichero(char *nombreFichero, char texto[][100], int numLineas) {
     fclose(f);
 }
 
-
 /* ========================================================= */
-/* FUNCION PARA CARGAR DATOS EN ESTRUCTURAS */
+/* FUNCION PARA CARGAR DATOS EN ESTRUCTURAS ESTATICAS        */
 /* ========================================================= */
 
-void gestionarFicheros(Puzle p[], int *np,
-                       Sala s[], int *ns,
-                       Conexion c[], int *nc,
-                       Objeto o[], int *no,
-                       Jugador j[], int *nj,
-                       int modo) {
+void gestionarFicheros(Puzle p[], int *np, Sala s[], int *ns, Conexion c[], int *nc, int modo) {
 
     FILE *f;
 
@@ -130,10 +61,10 @@ void gestionarFicheros(Puzle p[], int *np,
         f = fopen("Puzles.txt", "r");
 
         if (f != NULL) {
-            while (fscanf(f,"%3[^-]-%15[^-]-%d-%9[^-]-%150[^-]-%50[^\n]\n",
+            while (fscanf(f,"%3[^-]-%15[^-]-%d-%9[^-]-%150[^-]-%50[^-]-%14[^\n]\n",
                 p[*np].id, p[*np].nombre, &p[*np].id_sala,
-                p[*np].tipo, p[*np].descripcion, p[*np].solucion) == 6) {
-
+                p[*np].tipo, p[*np].descripcion, p[*np].solucion, p[*np].estado) == 7) {
+                
                 (*np)++;
             }
             fclose(f);
@@ -146,7 +77,6 @@ void gestionarFicheros(Puzle p[], int *np,
         if (f != NULL) {
             while (fscanf(f,"%d-%30[^-]-%9[^-]-%150[^\n]\n",
                 &s[*ns].id, s[*ns].nombre, s[*ns].tipo, s[*ns].descripcion) == 4) {
-
                 (*ns)++;
             }
             fclose(f);
@@ -157,38 +87,22 @@ void gestionarFicheros(Puzle p[], int *np,
         f = fopen("Conexiones.txt", "r");
 
         if (f != NULL) {
-            while (fscanf(f,"%3[^-]-%d-%d-%14[^-]-%9[^\n]\n",
-                c[*nc].id, &c[*nc].origen, &c[*nc].destino,
-                c[*nc].estado, c[*nc].condicion) == 5) {
+            while (fscanf(f, "%3[^-]-%d-%d-%14[^-]-%9[^\n]\n",
+             c[*nc].id, &c[*nc].origen, &c[*nc].destino, c[*nc].estado, c[*nc].condicion) == 5) {
+        
+       
+        for (int i = 0; i < *ns; i++) {
+            // Si el ID de la sala coincide con el origen de la conexión
+            if (s[i].id == c[*nc].origen) {
+                strcpy(c[*nc].nombre_origen, s[i].nombre);
+            }
+            // Si el ID de la sala coincide con el destino de la conexión
+            if (s[i].id == c[*nc].destino) {
+                strcpy(c[*nc].nombre_destino, s[i].nombre);
+            }
+        }
 
                 (*nc)++;
-            }
-            fclose(f);
-        }
-
-        /* ==================== OBJETOS ==================== */
-        *no = 0;
-        f = fopen("Objetos.txt", "r");
-
-        if (f != NULL) {
-            while (fscanf(f,"%4[^-]-%15[^-]-%50[^-]-%14[^\n]\n",
-                o[*no].id, o[*no].nombre, o[*no].descripcion,
-                o[*no].localizacion) == 4) {
-
-                (*no)++;
-            }
-            fclose(f);
-        }
-
-        /* ==================== JUGADORES ==================== */
-        *nj = 0;
-        f = fopen("Jugadores.txt", "r");
-
-        if (f != NULL) {
-            while (fscanf(f,"%d-%20[^-]-%10[^-]-%8[^\n]\n",
-                &j[*nj].id, j[*nj].nombre, j[*nj].usuario, j[*nj].password) == 4) {
-
-                (*nj)++;
             }
             fclose(f);
         }
